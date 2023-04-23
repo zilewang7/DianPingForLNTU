3b6aa273f03cfdd3d68188714f0a2757fda92dda
DDC <643063583@qq.com>
2023-04-23 21:27:47 +0800

    feat: user Info

--- components/UserView.tsx
+++ components/UserView.tsx
@@ -1,11 +1,12 @@
-import { Avatar } from '@rneui/themed';
+import { Avatar, Button, Divider } from '@rneui/themed';
 import React, { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import { MyAvatar } from './components/avatar';
 import { pickImage, uploadImg } from '../util/img';
 import { MyImageViewer } from './components/imgVIewer';
 import { useDispatch } from 'react-redux';
 import { updateUser } from '../redux/slices/userSlice';
+import { CommonActions, useNavigation } from '@react-navigation/native';
 
 interface UserInfo {
     isCurrentUser: boolean,
@@ -16,6 +17,7 @@ interface UserInfo {
 export function UserView({ isCurrentUser = false, username, avatarUrl }: UserInfo) {
     const [visible, setVisible] = useState(false);
     const dispatch = useDispatch();
+    const navigation = useNavigation();
 
     const pickAvatar = async () => {
         const uri = await pickImage(true);
@@ -26,25 +28,60 @@ export function UserView({ isCurrentUser = false, username, avatarUrl }: UserInf
         }
     };
 
+    const Info = ({ title, num = 0 }) =>
+    (<View style={styles.infoContainer}>
+        <Text style={styles.infoTitle}>{title}</Text>
+        <Text style={{padding:4}}>{num}</Text>
+    </View>)
+
     return (
-        <View>
-            <View style={styles.baseInfoContainer}>
-                <MyAvatar
-                    avatarUrl={avatarUrl}
-                    onAvatarPress={() => {
-                        setVisible(true)
-                    }}
-                >
-                    {isCurrentUser && <Avatar.Accessory size={18} onPress={pickAvatar} />}
-                </MyAvatar>
-                <Text>{username}</Text>
+        <Pressable onPress={() => {
+            if (isCurrentUser && username === '未登录') {
+                navigation.dispatch(
+                    CommonActions.navigate({
+                        name: '登录/注册',
+                        params: {
+                            screen: '登录/注册',
+                        },
+                    })
+                );
+            }
+        }}>
+            <View style={{ marginTop: 25 }}>
+                <View style={styles.baseInfoContainer}>
+                    <MyAvatar
+                        avatarUrl={avatarUrl}
+                        onAvatarPress={username !== '未登录' ? () => {
+                            if (avatarUrl) {
+                                setVisible(true)
+                            } else {
+                                pickAvatar()
+                            }
+                        } : undefined}
+                    >
+                        {isCurrentUser && username !== '未登录' && <Avatar.Accessory size={18} onPress={pickAvatar} />}
+                    </MyAvatar>
+                    <View style={styles.usernameContainer}>
+                        <Text style={styles.username}>{username}</Text>
+                        {isCurrentUser || <Button title='+关注' size="sm" containerStyle={{ marginHorizontal: 10 }} />}
+                    </View>
+                </View>
+                <MyImageViewer
+                    visible={visible}
+                    onCancel={setVisible.bind(this, false)}
+                    images={[{ url: avatarUrl }]}
+                />
             </View>
-            <MyImageViewer
-                visible={visible}
-                onCancel={setVisible.bind(this, false)}
-                images={[{ url: avatarUrl }]}
-            />
-        </View>
+            <View style={styles.vertical}>
+                <Info title='点评' />
+                <Divider orientation="vertical" width={2} />
+                <Info title='收藏' />
+                <Divider orientation="vertical" width={2} />
+                <Info title='关注' />
+                <Divider orientation="vertical" width={2} />
+                <Info title='粉丝' />
+            </View>
+        </Pressable>
     )
 };
 
@@ -53,5 +90,29 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: "space-evenly",
         alignItems: "center",
+    },
+    usernameContainer: {
+        flexDirection: 'row',
+        width: '50%',
+        height: '100%',
+        alignItems: "center",
+        justifyContent: 'center',
+    },
+    username: {
+        fontSize: 28,
+        fontWeight: 'bold',
+    },
+    vertical: {
+        padding: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+    infoContainer: {
+        alignItems: "center",
+        paddingVertical: 5,
+    },
+    infoTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
     },
 })
